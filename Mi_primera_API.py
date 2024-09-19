73b43238f55e550e7d1e746291281355b9db782f
aramis-lopez cruz <aramislopezcruz4@gmail.com>
2024-09-19 22:01:26 +0000

    update raadme

--- Mi_primera_API.py
+++ Mi_primera_API.py
@@ -4,10 +4,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/inicio")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/parametros/{nombre}")
+def get_parametros(nombre):
+    return {
+        "Nombre" : nombre
+    }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/incremento/{numero}")
+def get_incremento(numero:int):
+    numero = numero + 1
+    return {
+        "Numero" : numero
+    }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Mi_primera_API
+## Mi_primera_API
 primera_API
+
+## 1. Insatalacion de FastAPI
+---pip install "fastapi[standard]"---
+
+## 2. correr la API
+---fastapi dev main.py---
